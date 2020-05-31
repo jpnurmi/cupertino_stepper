fe3972107d857dde3aa9b44f658d7b8c5971f212
J-P Nurmi <jpnurmi@gmail.com>
2020-05-31 23:25:04 +0200

    Import Material Stepper from Flutter

--- lib/cupertino_stepper.dart
+++ lib/cupertino_stepper.dart
@@ -1,7 +1,8 @@
+// Copyright 2020 J-P Nurmi <jpnurmi@gmail.com>
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 library cupertino_stepper;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/stepper.dart';
